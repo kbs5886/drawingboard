aaeb62227030d7f374beea48e3bbdd620da0b636
Kartikey Singh <kbs5886@psu.edu>
2021-02-04 21:35:24 -0600

    basic http server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-asdasda
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const PORT = process.env.process || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(PORT, () => {
+  console.log('listening on *:3000');
+});
