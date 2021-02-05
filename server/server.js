4212199f1548bfbd50359ad274af993690090076
Kartikey Singh <kbs5886@psu.edu>
2021-02-04 21:40:38 -0600

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.process || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
 
 http.listen(PORT, () => {
-  console.log('listening on *:3000');
+  console.log(`listening on *:${PORT}`);
 });
